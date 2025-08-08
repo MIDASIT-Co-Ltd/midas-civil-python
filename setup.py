a061ef673404a9090df29059bd00851c33165aac
MIDAS-RnD <techsupport@midasit.com>
2025-08-08 11:28:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md','r') as f:
 
 
 setup(name='midas_civil',
-    version='0.2.0',
+    version='0.2.1',
     description='Python library for MIDAS Civil NX',
     author='Sumit Shekhar',
     author_email='sumit.midasit@gmail.com',
